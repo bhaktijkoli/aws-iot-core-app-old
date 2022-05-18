0803f9f5bef9948a55fd2df78c119773e7f93106
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-05-18 18:25:40 +0530

    feat: added create thing

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("Hello App")
+import os
+import boto3
+from dotenv import load_dotenv
+load_dotenv()
+
+aws_region = os.getenv("AWS_REGION")
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_thing_name = os.getenv("AWS_THING_NAME")
+aws_thing_type_name = os.getenv("AWS_THING_TYPE_NAME")
+
+client = boto3.client("iot",
+                      region_name=aws_region,
+                      aws_access_key_id=aws_access_key_id,
+                      aws_secret_access_key=aws_secret_access_key
+                      )
+
+response = client.create_thing(
+    thingName=aws_thing_name,
+    thingTypeName=aws_thing_type_name,
+)
+
+print(response)
