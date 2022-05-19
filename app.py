e42e083b05320e788caa314a34c4af5a5b047b28
Bhaktij Koli <bhaktijkoli121@gmail.com>
2022-05-19 19:07:05 +0530

    chore: removed thing registration

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-import os
-import boto3
-from dotenv import load_dotenv
-load_dotenv()
+# import os
+# import boto3
+# from dotenv import load_dotenv
+# load_dotenv()
 
-aws_region = os.getenv("AWS_REGION")
-aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-aws_thing_name = os.getenv("AWS_THING_NAME")
-aws_thing_type_name = os.getenv("AWS_THING_TYPE_NAME")
+# aws_region = os.getenv("AWS_REGION")
+# aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+# aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+# aws_thing_name = os.getenv("AWS_THING_NAME")
+# aws_thing_type_name = os.getenv("AWS_THING_TYPE_NAME")
 
-client = boto3.client("iot",
-                      region_name=aws_region,
-                      aws_access_key_id=aws_access_key_id,
-                      aws_secret_access_key=aws_secret_access_key
-                      )
+# client = boto3.client("iot",
+#                       region_name=aws_region,
+#                       aws_access_key_id=aws_access_key_id,
+#                       aws_secret_access_key=aws_secret_access_key
+#                       )
 
-response = client.create_thing(
-    thingName=aws_thing_name,
-    thingTypeName=aws_thing_type_name,
-)
+# response = client.create_thing(
+#     thingName=aws_thing_name,
+#     thingTypeName=aws_thing_type_name,
+# )
 
-print(response)
+# print(response)
+print("Hello from iot App")
